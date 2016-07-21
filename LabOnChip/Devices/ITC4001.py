b7c8919ca1b3e29462ed696fad0817b368a493b8
Thomas Mann <mn14tm@leeds.ac.uk>
2016-07-21 16:34:54 +0100

    ITC setup and more analysis

--- LabOnChip/Devices/ITC4001.py
+++ LabOnChip/Devices/ITC4001.py
@@ -9,10 +9,34 @@ class ITC4001:
         self.inst = rm.open_resource('GPIB0::12::INSTR')
         print(self.inst.query("*IDN?"))  # What are you?
 
-    # def setup(self):
+    def setup(self):
+        # Set TEC setpoint
+        self.inst.write('SOUR:TEMP 25C')
+
+    def save_config(self):
+        self.inst.write('*SAV 1')
+
+    def load_config(self):
+        self.inst.write('*RCL 1')
+
+    def turn_ld_on(self):
+        self.inst.write('OUTP ON')
+
+    def turn_ld_off(self):
+        self.inst.write('OUTP OFF')
 
     def get_optical_power(self):
-        self.inst.query("")
+        # Measures LD power via PD
+        return self.inst.query("MEAS:POWer2?")
+
+    def set_qcw(self):
+        # Set source period to 200 ms
+        self.inst.write('SOUR:PULS:PER 0.2')
+        # Set source period to 50 ms
+        self.inst.write('SOUR:PULS:PER 0.05')
+        # Set trigger source to internal
+        self.inst.write('TRIG:SOUR INT')
+
 
 
 if __name__ == "__main__":
